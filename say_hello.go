012bab1a6ca7adf46f53069e9441a9781082be74
tantowi.setiawan <tantowi.setiawan@binus.ac.id>
2024-04-07 11:59:55 +0700

    GO-MODULE: Modified SayHello function

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,8 @@
 package go_say_hello
 
-func SayHello(name string) string {
-	return "Hello, " + name
+func SayHello(name string) (number int, message string) {
+	number = 1
+	message = "Hello, " + name
+
+	return number, message
 }
