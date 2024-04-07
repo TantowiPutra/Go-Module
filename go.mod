b8ea0d14260bf28bf37ca99e2e5226e06ee771e3
tantowi.setiawan <tantowi.setiawan@binus.ac.id>
2024-04-07 12:16:16 +0700

    G0-MODULE: Major Changes Added Second Name Parameter

--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
-module github.com/TantowiPutra/Go-Module
+// module github.com/TantowiPutra/Go-Module
+module github.com/TantowiPutra/Go-Module/v2
 
 go 1.19
--- say_hello.go
+++ say_hello.go
@@ -1,8 +1,14 @@
 package go_say_hello
 
-func SayHello(name string) (number int, message string) {
-	number = 1
-	message = "Hello, " + name
+// func SayHello(name string) (number int, message string) {
+// 	number = 1
+// 	message = "Hello, " + name
 
-	return number, message
+// 	return number, message
+// }
+
+// ! Major Changes
+
+func SayHello(first_name string, second_name string) string{
+	return "Hello, " + first_name + " " + second_name
 }
